5ea0de01dfe9d529009b0f57d8c46297e1e5dfc4
silvanaolmedo <vagrant@kleer-csd-box>
2016-12-15 17:44:06 -0300

    Mostrar una suma,'+' cambiado por 'mas'

--- features/peaches.feature
+++ features/peaches.feature
@@ -4,3 +4,8 @@ Scenario: portada
 	Given voy a la pantalla inicial
 	Then veo "Peaches Math"
 
+Scenario: mostrar una suma
+	Given voy a la pantalla con suma
+	Then veo "4 mas 7"
+
+
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -2,7 +2,18 @@ Given(/^voy a la pantalla inicial$/) do
   visit '/'
 end
 
-Then(/^veo "(.*?)"$/) do |titulo|
-  last_response.body.should =~ /#{titulo}/m
+Given(/^voy a la pantalla con suma$/) do
+	visit '/'
 end
 
+#Then(/^veo "(.*?)"$/) do |titulo|
+  #last_response.body.should =~ /#{titulo}/m
+#end
+
+Then(/^veo "(.*?)"$/) do |arg1|
+	last_response.body.should =~ /#{arg1}/m
+end
+
+Then(/^veo numero (\d+)$/) do |arg1|
+	last_response.body.should =~ /#{arg1}/m
+end
