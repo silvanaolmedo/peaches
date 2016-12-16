d60577fca130ed2139a88e26fc1d893a8ad605f8
monicaHH <vagrant@kleer-csd-box>
2016-12-16 11:46:41 -0300

    Calcular operacion + Verificar respuesta

--- lib/peaches.rb
+++ lib/peaches.rb
@@ -1,6 +1,12 @@
 class Peaches
 	def initialize
 		@puntajeA = 0
+                @operador1 = 0
+                @operador2 = 0
+                @operacion = 1
+                @resultado = 0
+               
+                
 	end
 
 	def puntaje
@@ -10,4 +16,22 @@ class Peaches
 	def generar_operacion
 		"4+5"
 	end
+        def calcular_operacion 
+           if @operacion == 1 
+             @resultado= @operador1 + @operador2
+             
+           else
+              @resultado= @operador1 * @operador2
+           end
+        end
+
+        def verificar_respuesta respuesta
+           if respuesta == @resultado
+              @devolucion = "Respuesta correcta"
+           else
+             @devolucion = "Respuesta incorrecta"
+    	   end
+        end
+          
+            
 end
--- spec/peaches_spec.rb
+++ spec/peaches_spec.rb
@@ -11,4 +11,15 @@ describe "Juego Peaches Math" do
 		game = Peaches.new
 		expect(game.generar_operacion).to eq("4+5")
 	end
+        
+     	it "Calcular operacion" do
+             game = Peaches.new
+             expect(game.calcular_operacion).to eq(9)
+        end
+	
+	it "Verificar respuesta" do
+  	    game = Peaches.new
+     	    game.calcular_operacion 
+	    expect(game.verificar_respuesta 9).to eq "Respuesta correcta"
+     	end
 end
